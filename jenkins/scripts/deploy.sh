ae98836c414c24b77e2546030cdf63295e39fb31
Jevan <jevan@Jevans-MacBook-Pro.local>
2022-11-16 05:24:19 +0800

    lol

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-set -x
+#set -x
 docker run -d -p 80:80 --name my-apache-php-app -v https://github.com/jevng/PHPSelenium.git
 sleep 1
-set +x
+#set +x
 
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
